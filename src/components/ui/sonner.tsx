4610fcb2757c1e30d9c769b4976c24e1c5b967ec
Bhavya <bhavyaagarwal00000@gmail.com>
2025-11-27 01:16:57 +0530

    done11

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -2,9 +2,7 @@ import { Toaster as Sonner, toast } from "sonner";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
-const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
-
+const Toaster = ({ theme = "system", ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
